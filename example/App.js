d9c0600f79f13daebbe9c8d98388ff96213782b7
Johannes Sorg <johannes@sorg.me>
2019-05-05 11:36:50 +0200

    fixes eslint setup

--- example/App.js
+++ example/App.js
@@ -1,53 +1,53 @@
-import React, { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
-import RNStoryShare from "react-native-story-share";
+import React, { useEffect, useState } from 'react'
+import { Button, Text, View } from 'react-native'
+import RNStoryShare from 'react-native-story-share'
 
 const base64Background =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAQAAAAHUWYVAAABKUlEQVR42u3RMQEAAAjDMOZf9DDBwZFKaNKOHhUgQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQICYAERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABAQIECACAkRAgAgIEAEBIiACAkRAgAgIEAEBIiACAkRAgOiyBdIBj0hI3a/GAAAAAElFTkSuQmCC";
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAQAAAAHUWYVAAABKUlEQVR42u3RMQEAAAjDMOZf9DDBwZFKaNKOHhUgQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQICYAERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABAQIECACAkRAgAgIEAEBIiACAkRAgAgIEAEBIiACAkRAgOiyBdIBj0hI3a/GAAAAAElFTkSuQmCC'
 const base64Sticker =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABn0lEQVR42u3TAQ0AAAjDMO5fNCAABaTNLCxdtQGXGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEDGIQMAgYBAwCBgGDgEHAIGAQMAhgEDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBDAIGAQMAgYBAwCBgGDgEHAIGAQwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAggEHAIGAQMAgYBAwCBgGDgEHAIAYBg4BBwCBgEDAIGAQMAgYBgwAGAYOAQcAgYBAwCBgEDAIGAYMYBAwCBgGDgEHAIGAQMAgYBAwCGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEMAgYBAwCBgGDgEHAIGAQMAgYBDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBAwCGAQMAgYBAwCBgGDgEHAIGAQMIhBwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAgBgGDgEHAIGAQMAgYBAwCHw2n4Y9IKdr/IQAAAABJRU5ErkJggg==";
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABn0lEQVR42u3TAQ0AAAjDMO5fNCAABaTNLCxdtQGXGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEDGIQMAgYBAwCBgGDgEHAIGAQMAhgEDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBDAIGAQMAgYBAwCBgGDgEHAIGAQwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAggEHAIGAQMAgYBAwCBgGDgEHAIAYBg4BBwCBgEDAIGAQMAgYBgwAGAYOAQcAgYBAwCBgEDAIGAYMYBAwCBgGDgEHAIGAQMAgYBAwCGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEMAgYBAwCBgGDgEHAIGAQMAgYBDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBAwCGAQMAgYBAwCBgGDgEHAIGAQMIhBwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAgBgGDgEHAIGAQMAgYBAwCHw2n4Y9IKdr/IQAAAABJRU5ErkJggg=='
 
 export const App = () => {
-  const [isInstagramAvailable, setIsInstagramAvailable] = useState(false);
-  const [error, setError] = useState(null);
+  const [isInstagramAvailable, setIsInstagramAvailable] = useState(false)
+  const [error, setError] = useState(null)
 
   const onPressShareInstagram = () => {
-    setError(null);
+    setError(null)
 
     RNStoryShare.shareToInstagram({
       backgroundAsset: base64Background,
       stickerAsset: base64Sticker,
-      attributionLink: "",
-      type: RNStoryShare.BASE64
-    }).catch(setError);
-  };
+      attributionLink: '',
+      type: RNStoryShare.BASE64,
+    }).catch(setError)
+  }
 
   const onPressShareSnapchat = () => {
-    setError(null);
+    setError(null)
 
     RNStoryShare.shareToSnapchat({
       backgroundAsset: base64Background,
       stickerAsset: base64Sticker,
-      attributionLink: "",
-      type: RNStoryShare.BASE64
-    }).catch(setError);
-  };
+      attributionLink: '',
+      type: RNStoryShare.BASE64,
+    }).catch(setError)
+  }
 
   useEffect(() => {
     RNStoryShare.isInstagramAvailable()
       .then(setIsInstagramAvailable)
-      .catch(e => console.error(e));
-  }, []);
+      .catch(e => console.error(e))
+  }, [])
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 24 }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 24 }}>
         RNStoryShare Example
       </Text>
       <Text style={{ marginBottom: 24 }}>
-        Instagram is{" "}
-        <Text style={{ fontWeight: "bold" }}>
-          {isInstagramAvailable ? "available" : "not available"}
+        Instagram is{' '}
+        <Text style={{ fontWeight: 'bold' }}>
+          {isInstagramAvailable ? 'available' : 'not available'}
         </Text>
       </Text>
       <Button title="Share to Instagram" onPress={onPressShareInstagram} />
@@ -60,5 +60,5 @@ export const App = () => {
         {error ? `error: ${error.message}` : null}
       </Text>
     </View>
-  );
-};
+  )
+}
