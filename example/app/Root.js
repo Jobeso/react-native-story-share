9deffb59d2746f744602aed1223adefd191e6351
Johannes Sorg <johannes@sorg.me>
2019-05-11 18:28:58 +0200

    updates example

--- example/app/Root.js
+++ example/app/Root.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useRef, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
-import RNStoryShare from "react-native-story-share";
-import ViewShot, { captureRef } from "react-native-view-shot";
-import Permissions from "react-native-permissions";
-import { curry } from "ramda";
-import { Button } from "./Button";
-import { ShareSection } from "./ShareSection";
+import { curry } from 'ramda'
+import React from 'react'
+import { Platform, ScrollView, Text } from 'react-native'
+import Permissions from 'react-native-permissions'
+import RNStoryShare from 'react-native-story-share'
+import ViewShot, { captureRef } from 'react-native-view-shot'
+import { Button } from './Button'
+import { ShareSection } from './ShareSection'
 
 const base64Background =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAQAAAAHUWYVAAABKUlEQVR42u3RMQEAAAjDMOZf9DDBwZFKaNKOHhUgQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQICYAERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABAQIECACAkRAgAgIEAEBIiACAkRAgAgIEAEBIiACAkRAgOiyBdIBj0hI3a/GAAAAAElFTkSuQmCC";
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAQAAAAHUWYVAAABKUlEQVR42u3RMQEAAAjDMOZf9DDBwZFKaNKOHhUgQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQICYAERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABAQIECACAkRAgAgIEAEBIiACAkRAgAgIEAEBIiACAkRAgOiyBdIBj0hI3a/GAAAAAElFTkSuQmCC'
 const base64Sticker =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABn0lEQVR42u3TAQ0AAAjDMO5fNCAABaTNLCxdtQGXGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEDGIQMAgYBAwCBgGDgEHAIGAQMAhgEDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBDAIGAQMAgYBAwCBgGDgEHAIGAQwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAggEHAIGAQMAgYBAwCBgGDgEHAIAYBg4BBwCBgEDAIGAQMAgYBgwAGAYOAQcAgYBAwCBgEDAIGAYMYBAwCBgGDgEHAIGAQMAgYBAwCGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEMAgYBAwCBgGDgEHAIGAQMAgYBDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBAwCGAQMAgYBAwCBgGDgEHAIGAQMIhBwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAgBgGDgEHAIGAQMAgYBAwCHw2n4Y9IKdr/IQAAAABJRU5ErkJggg==";
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABn0lEQVR42u3TAQ0AAAjDMO5fNCAABaTNLCxdtQGXGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEDGIQMAgYBAwCBgGDgEHAIGAQMAhgEDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBDAIGAQMAgYBAwCBgGDgEHAIGAQwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAggEHAIGAQMAgYBAwCBgGDgEHAIAYBg4BBwCBgEDAIGAQMAgYBgwAGAYOAQcAgYBAwCBgEDAIGAYMYBAwCBgGDgEHAIGAQMAgYBAwCGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEMAgYBAwCBgGDgEHAIGAQMAgYBDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBAwCGAQMAgYBAwCBgGDgEHAIGAQMIhBwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAgBgGDgEHAIGAQMAgYBAwCHw2n4Y9IKdr/IQAAAABJRU5ErkJggg=='
 
 // curry is not necessary RNStoryShares functions
 // it returns a new function when only part of the arguments are provided
@@ -19,12 +19,12 @@ const shareInstagram = curry((onError, type, backgroundAsset, stickerAsset) => {
   RNStoryShare.shareToInstagram({
     backgroundAsset,
     stickerAsset,
-    backgroundBottomColor: "#f44162",
-    backgroundTopColor: "#f4a142",
-    attributionLink: "",
-    type
-  }).catch(onError);
-});
+    backgroundBottomColor: '#f44162',
+    backgroundTopColor: '#f4a142',
+    attributionLink: '',
+    type,
+  }).catch(onError)
+})
 
 // curry is not necessary RNStoryShares functions
 // it returns a new function when only part of the arguments are provided
@@ -33,134 +33,133 @@ const shareSnapchat = curry((onError, type, backgroundAsset, stickerAsset) => {
   RNStoryShare.shareToSnapchat({
     backgroundAsset,
     stickerAsset,
-    attributionLink: "https://google.com/",
-    type
-  }).catch(onError);
-});
+    attributionLink: 'https://google.com/',
+    type,
+  }).catch(onError)
+})
 
 class Root extends React.PureComponent {
   state = {
     error: undefined,
     isInstagramAvailable: false,
-    isSnapchatAvailable: false
-  };
-
-  _setIsInstagramAvailable = isAvailable => {
-    this.setState({ isInstagramAvailable: isAvailable });
-  };
-  _setIsSnapchatAvailable = isAvailable => {
-    this.setState({ isSnapchatAvailable: isAvailable });
-  };
-  _setError = e => {
-    this.setState({ error: e });
-  };
+    isSnapchatAvailable: false,
+  }
 
   async componentDidMount() {
-    Permissions.request("storage").then(response => {
-      if (response === "denied") {
-        this.setState({ error: "No permissions" });
-      }
-    });
+    if (Platform.OS === 'android') {
+      Permissions.request('storage').then(response => {
+        if (response === 'denied') {
+          this.setState({ error: 'No permissions' })
+        }
+      })
+    }
 
     try {
-      const _isInstagramAvailable = await RNStoryShare.isInstagramAvailable();
-      const _isSnapchatAvailable = await RNStoryShare.isSnapchatAvailable();
+      const isInstagramAvailable = await RNStoryShare.isInstagramAvailable()
+      const isSnapchatAvailable = await RNStoryShare.isSnapchatAvailable()
 
-      this._setIsInstagramAvailable(_isInstagramAvailable);
-      this._setIsSnapchatAvailable(_isSnapchatAvailable);
-    } catch (e) {
-      this._setError(e);
+      this.setState({ isInstagramAvailable, isSnapchatAvailable })
+    } catch (error) {
+      this.setState({ error })
     }
   }
 
-  _ref = undefined;
+  _ref = undefined
+
+  _setError = e => {
+    this.setState({ error: e })
+  }
 
   _captureScreen = () =>
     new Promise(resolve => {
       captureRef(this._ref, {
-        format: "png",
-        quality: 0.8
+        format: 'png',
+        quality: 0.8,
       })
         .then(uri => {
-          resolve(uri);
+          resolve(uri)
         })
-        .catch(this._setError);
-    });
+        .catch(this._setError)
+    })
 
-  _shareInstaBase64 = shareInstagram(this._setError, RNStoryShare.BASE64);
+  _shareInstaBase64 = shareInstagram(this._setError, RNStoryShare.BASE64)
   _shareInstaBackground = () => {
-    this._shareInstaBase64(base64Background, undefined);
-  };
+    this._shareInstaBase64(base64Background, undefined)
+  }
   _shareInstaSticker = () => {
-    this._shareInstaBase64(undefined, base64Sticker);
-  };
+    this._shareInstaBase64(undefined, base64Sticker)
+  }
   _shareInstaBoth = () => {
-    this._shareInstaBase64(base64Background, base64Sticker);
-  };
+    this._shareInstaBase64(base64Background, base64Sticker)
+  }
 
-  _shareInstaFile = shareInstagram(this._setError, RNStoryShare.FILE);
+  _shareInstaFile = shareInstagram(this._setError, RNStoryShare.FILE)
   _shareInstaFileBackground = async () => {
-    const uri = await this._captureScreen();
+    const uri = await this._captureScreen()
 
-    this._shareInstaFile(uri, undefined);
-  };
+    this._shareInstaFile(uri, undefined)
+  }
   _shareInstaFileSticker = async () => {
-    const uri = await this._captureScreen();
+    const uri = await this._captureScreen()
 
-    this._shareInstaFile(undefined, uri);
-  };
+    this._shareInstaFile(undefined, uri)
+  }
   _shareInstaFileBoth = async () => {
-    const uri = await this._captureScreen();
+    const uri = await this._captureScreen()
 
-    this._shareInstaFile(uri, uri);
-  };
+    this._shareInstaFile(uri, uri)
+  }
 
-  _shareSnapBase64 = shareSnapchat(this._setError, RNStoryShare.BASE64);
+  _shareSnapBase64 = shareSnapchat(this._setError, RNStoryShare.BASE64)
   _shareSnapBackground = () => {
-    this._shareSnapBase64(base64Background, undefined);
-  };
+    this._shareSnapBase64(base64Background, undefined)
+  }
   _shareSnapSticker = () => {
-    this._shareSnapBase64(undefined, base64Sticker);
-  };
+    this._shareSnapBase64(undefined, base64Sticker)
+  }
   _shareSnapBoth = () => {
-    this._shareSnapBase64(base64Background, base64Sticker);
-  };
+    this._shareSnapBase64(base64Background, base64Sticker)
+  }
 
-  _shareSnapFile = shareSnapchat(this._setError, RNStoryShare.FILE);
+  _shareSnapFile = shareSnapchat(this._setError, RNStoryShare.FILE)
   _shareSnapFileBackground = async () => {
-    const uri = await this._captureScreen();
+    const uri = await this._captureScreen()
 
-    this._shareSnapFile(uri, undefined);
-  };
+    this._shareSnapFile(uri, undefined)
+  }
   _shareSnapFileSticker = async () => {
-    const uri = await this._captureScreen();
+    const uri = await this._captureScreen()
 
-    this._shareSnapFile(undefined, uri);
-  };
+    this._shareSnapFile(undefined, uri)
+  }
   _shareSnapFileBoth = async () => {
-    const uri = await this._captureScreen();
+    const uri = await this._captureScreen()
 
-    this._shareSnapFile(uri, uri);
-  };
+    this._shareSnapFile(uri, uri)
+  }
+
+  _setRef = ref => {
+    this._ref = ref
+  }
 
   render() {
-    const { error, isInstagramAvailable, isSnapchatAvailable } = this.state;
+    const { error, isInstagramAvailable, isSnapchatAvailable } = this.state
 
     return (
       <ScrollView
         contentContainerStyle={{
-          alignItems: "center",
+          alignItems: 'center',
           paddingVertical: 64,
-          paddingHorizontal: 16
+          paddingHorizontal: 16,
         }}
         style={{ flex: 1 }}
       >
-        <ViewShot ref={r => (this._ref = r)}>
+        <ViewShot ref={this._setRef}>
           <Text
             style={{
-              fontWeight: "bold",
+              fontWeight: 'bold',
               fontSize: 24,
-              marginVertical: 64
+              marginVertical: 64,
             }}
           >
             RNStoryShare Example
@@ -204,8 +203,8 @@ class Root extends React.PureComponent {
           {error ? `error: ${error.message}` : null}
         </Text>
       </ScrollView>
-    );
+    )
   }
 }
 
-export { Root };
+export { Root }
