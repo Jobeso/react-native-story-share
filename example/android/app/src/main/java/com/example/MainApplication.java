5afbff7041656f4e435d8907055f9a49248f60ce
Johannes Sorg <johannes@sorg.me>
2019-05-11 17:35:26 +0200

    updates example to represent capabilities

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.jobeso.RNStorySharePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNViewShotPackage(),
             new RNStorySharePackage()
       );
     }
--- example/app/Root.js
+++ example/app/Root.js
@@ -1,99 +1,211 @@
-import React, { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
-import RNStoryShare from 'react-native-story-share'
-import { Button } from './Button'
-import { ShareSection } from './ShareSection'
+import React, { useEffect, useRef, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import RNStoryShare from "react-native-story-share";
+import ViewShot, { captureRef } from "react-native-view-shot";
+import Permissions from "react-native-permissions";
+import { curry } from "ramda";
+import { Button } from "./Button";
+import { ShareSection } from "./ShareSection";
 
 const base64Background =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAQAAAAHUWYVAAABKUlEQVR42u3RMQEAAAjDMOZf9DDBwZFKaNKOHhUgQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQICYAERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABAQIECACAkRAgAgIEAEBIiACAkRAgAgIEAEBIiACAkRAgOiyBdIBj0hI3a/GAAAAAElFTkSuQmCC'
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAQAAAAHUWYVAAABKUlEQVR42u3RMQEAAAjDMOZf9DDBwZFKaNKOHhUgQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQICYAERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABERAgAgIEAEBIiBABAQIECACAkRAgAgIEAEBIiACAkRAgAgIEAEBIiACAkRAgOiyBdIBj0hI3a/GAAAAAElFTkSuQmCC";
 const base64Sticker =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABn0lEQVR42u3TAQ0AAAjDMO5fNCAABaTNLCxdtQGXGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEDGIQMAgYBAwCBgGDgEHAIGAQMAhgEDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBDAIGAQMAgYBAwCBgGDgEHAIGAQwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAggEHAIGAQMAgYBAwCBgGDgEHAIAYBg4BBwCBgEDAIGAQMAgYBgwAGAYOAQcAgYBAwCBgEDAIGAYMYBAwCBgGDgEHAIGAQMAgYBAwCGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEMAgYBAwCBgGDgEHAIGAQMAgYBDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBAwCGAQMAgYBAwCBgGDgEHAIGAQMIhBwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAgBgGDgEHAIGAQMAgYBAwCHw2n4Y9IKdr/IQAAAABJRU5ErkJggg=='
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABn0lEQVR42u3TAQ0AAAjDMO5fNCAABaTNLCxdtQGXGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEDGIQMAgYBAwCBgGDgEHAIGAQMAhgEDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBDAIGAQMAgYBAwCBgGDgEHAIGAQwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAggEHAIGAQMAgYBAwCBgGDgEHAIAYBg4BBwCBgEDAIGAQMAgYBgwAGAYOAQcAgYBAwCBgEDAIGAYMYBAwCBgGDgEHAIGAQMAgYBAwCGAQMAgYBg4BBwCBgEDAIGAQMAhgEDAIGAYOAQcAgYBAwCBgEMAgYBAwCBgGDgEHAIGAQMAgYBDAIGAQMAgYBg4BBwCBgEDAIYBAwCBgEDAIGAYOAQcAgYBAwCGAQMAgYBAwCBgGDgEHAIGAQMIhBwCBgEDAIGAQMAgYBg4BBwCCAQcAgYBAwCBgEDAIGAYOAQcAgBgGDgEHAIGAQMAgYBAwCHw2n4Y9IKdr/IQAAAABJRU5ErkJggg==";
 
-const shareInstagram = (onError, backgroundAsset, stickerAsset) => () => {
+// curry is not necessary RNStoryShares functions
+// it returns a new function when only part of the arguments are provided
+// see https://ramdajs.com/docs/#curry
+const shareInstagram = curry((onError, type, backgroundAsset, stickerAsset) => {
   RNStoryShare.shareToInstagram({
     backgroundAsset,
     stickerAsset,
-    attributionLink: 'https://github.com/Jobeso/react-native-story-share',
-    type: RNStoryShare.BASE64,
-  }).catch(onError)
-}
+    backgroundBottomColor: "#f44162",
+    backgroundTopColor: "#f4a142",
+    attributionLink: "",
+    type
+  }).catch(onError);
+});
 
-const shareSnapchat = (onError, backgroundAsset, stickerAsset) => () => {
+// curry is not necessary RNStoryShares functions
+// it returns a new function when only part of the arguments are provided
+// see https://ramdajs.com/docs/#curry
+const shareSnapchat = curry((onError, type, backgroundAsset, stickerAsset) => {
   RNStoryShare.shareToSnapchat({
     backgroundAsset,
     stickerAsset,
-    attributionLink: 'https://github.com/Jobeso/react-native-story-share',
-    type: RNStoryShare.BASE64,
-  }).catch(onError)
-}
+    attributionLink: "https://google.com/",
+    type
+  }).catch(onError);
+});
 
-export const Root = () => {
-  const [isInstagramAvailable, setIsInstagramAvailable] = useState(false)
-  const [isSnapchatAvailable, setIsSnapchatAvailable] = useState(false)
-  const [error, setError] = useState(null)
+class Root extends React.PureComponent {
+  state = {
+    error: undefined,
+    isInstagramAvailable: false,
+    isSnapchatAvailable: false
+  };
 
-  const shareInstagramBackground = shareInstagram(setError, base64Background)
-  const shareInstagramSticker = shareInstagram(
-    setError,
-    undefined,
-    base64Sticker
-  )
-  const shareInstagramBoth = shareInstagram(
-    setError,
-    base64Background,
-    base64Sticker
-  )
-  const shareSnapchatBackground = shareSnapchat(setError, base64Background)
-  const shareSnapchatSticker = shareSnapchat(setError, undefined, base64Sticker)
-  const shareSnapchatBoth = shareSnapchat(
-    setError,
-    base64Background,
-    base64Sticker
-  )
+  _setIsInstagramAvailable = isAvailable => {
+    this.setState({ isInstagramAvailable: isAvailable });
+  };
+  _setIsSnapchatAvailable = isAvailable => {
+    this.setState({ isSnapchatAvailable: isAvailable });
+  };
+  _setError = e => {
+    this.setState({ error: e });
+  };
 
-  useEffect(() => {
-    const checkAvailability = async () => {
-      try {
-        const _isInstagramAvailable = await RNStoryShare.isInstagramAvailable()
-        const _isSnapchatAvailable = await RNStoryShare.isSnapchatAvailable()
-
-        setIsInstagramAvailable(_isInstagramAvailable)
-        setIsSnapchatAvailable(_isSnapchatAvailable)
-      } catch (e) {
-        setError(e)
+  async componentDidMount() {
+    Permissions.request("storage").then(response => {
+      if (response === "denied") {
+        this.setState({ error: "No permissions" });
       }
+    });
+
+    try {
+      const _isInstagramAvailable = await RNStoryShare.isInstagramAvailable();
+      const _isSnapchatAvailable = await RNStoryShare.isSnapchatAvailable();
+
+      this._setIsInstagramAvailable(_isInstagramAvailable);
+      this._setIsSnapchatAvailable(_isSnapchatAvailable);
+    } catch (e) {
+      this._setError(e);
     }
+  }
 
-    checkAvailability()
-  }, [])
+  _ref = undefined;
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', paddingVertical: 64 }}>
-      <Text
-        style={{
-          fontWeight: 'bold',
-          fontSize: 24,
-          marginVertical: 64,
+  _captureScreen = () =>
+    new Promise(resolve => {
+      captureRef(this._ref, {
+        format: "png",
+        quality: 0.8
+      })
+        .then(uri => {
+          resolve(uri);
+        })
+        .catch(this._setError);
+    });
+
+  _shareInstaBase64 = shareInstagram(this._setError, RNStoryShare.BASE64);
+  _shareInstaBackground = () => {
+    this._shareInstaBase64(base64Background, undefined);
+  };
+  _shareInstaSticker = () => {
+    this._shareInstaBase64(undefined, base64Sticker);
+  };
+  _shareInstaBoth = () => {
+    this._shareInstaBase64(base64Background, base64Sticker);
+  };
+
+  _shareInstaFile = shareInstagram(this._setError, RNStoryShare.FILE);
+  _shareInstaFileBackground = async () => {
+    const uri = await this._captureScreen();
+
+    this._shareInstaFile(uri, undefined);
+  };
+  _shareInstaFileSticker = async () => {
+    const uri = await this._captureScreen();
+
+    this._shareInstaFile(undefined, uri);
+  };
+  _shareInstaFileBoth = async () => {
+    const uri = await this._captureScreen();
+
+    this._shareInstaFile(uri, uri);
+  };
+
+  _shareSnapBase64 = shareSnapchat(this._setError, RNStoryShare.BASE64);
+  _shareSnapBackground = () => {
+    this._shareSnapBase64(base64Background, undefined);
+  };
+  _shareSnapSticker = () => {
+    this._shareSnapBase64(undefined, base64Sticker);
+  };
+  _shareSnapBoth = () => {
+    this._shareSnapBase64(base64Background, base64Sticker);
+  };
+
+  _shareSnapFile = shareSnapchat(this._setError, RNStoryShare.FILE);
+  _shareSnapFileBackground = async () => {
+    const uri = await this._captureScreen();
+
+    this._shareSnapFile(uri, undefined);
+  };
+  _shareSnapFileSticker = async () => {
+    const uri = await this._captureScreen();
+
+    this._shareSnapFile(undefined, uri);
+  };
+  _shareSnapFileBoth = async () => {
+    const uri = await this._captureScreen();
+
+    this._shareSnapFile(uri, uri);
+  };
+
+  render() {
+    const { error, isInstagramAvailable, isSnapchatAvailable } = this.state;
+
+    return (
+      <ScrollView
+        contentContainerStyle={{
+          alignItems: "center",
+          paddingVertical: 64,
+          paddingHorizontal: 16
         }}
+        style={{ flex: 1 }}
       >
-        RNStoryShare Example
-      </Text>
+        <ViewShot ref={r => (this._ref = r)}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 24,
+              marginVertical: 64
+            }}
+          >
+            RNStoryShare Example
+          </Text>
 
-      <ShareSection title="Instagram" isAvailable={isInstagramAvailable}>
-        <Button onPress={shareInstagramBackground}>Background</Button>
-        <Button onPress={shareInstagramSticker}>Sticker</Button>
-        <Button onPress={shareInstagramBoth}>Both</Button>
-      </ShareSection>
+          <ShareSection
+            title="Instagram Base64"
+            isAvailable={isInstagramAvailable}
+          >
+            <Button onPress={this._shareInstaBackground}>Background</Button>
+            <Button onPress={this._shareInstaSticker}>Sticker</Button>
+            <Button onPress={this._shareInstaBoth}>Both</Button>
+          </ShareSection>
 
-      <ShareSection title="Snapchat" isAvailable={isSnapchatAvailable}>
-        <Button onPress={shareSnapchatBackground}>Background</Button>
-        <Button onPress={shareSnapchatSticker}>Sticker</Button>
-        <Button onPress={shareSnapchatBoth}>Both</Button>
-      </ShareSection>
+          <ShareSection
+            title="Instagram File"
+            isAvailable={isInstagramAvailable}
+          >
+            <Button onPress={this._shareInstaFileBackground}>Background</Button>
+            <Button onPress={this._shareInstaFileSticker}>Sticker</Button>
+            <Button onPress={this._shareInstaFileBoth}>Both</Button>
+          </ShareSection>
 
-      <Text style={{ marginTop: 48, minHeight: 80 }}>
-        {error ? `error: ${error.message}` : null}
-      </Text>
-    </View>
-  )
+          <ShareSection
+            title="Snapchat Base64"
+            isAvailable={isSnapchatAvailable}
+          >
+            <Button onPress={this._shareSnapBackground}>Background</Button>
+            <Button onPress={this._shareSnapSticker}>Sticker</Button>
+            <Button onPress={this._shareSnapBoth}>Both</Button>
+          </ShareSection>
+
+          <ShareSection title="Snapchat File" isAvailable={isSnapchatAvailable}>
+            <Button onPress={this._shareSnapFileBackground}>Background</Button>
+            <Button onPress={this._shareSnapFileSticker}>Sticker</Button>
+            <Button onPress={this._shareSnapFileBoth}>Both</Button>
+          </ShareSection>
+        </ViewShot>
+
+        <Text style={{ marginTop: 48, minHeight: 80 }}>
+          {error ? `error: ${error.message}` : null}
+        </Text>
+      </ScrollView>
+    );
+  }
 }
+
+export { Root };
