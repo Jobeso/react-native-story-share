45887592c67b64e2f217fe8d607fe074f7c36acc
franznkemaka <franz@rigle.co>
2020-07-22 16:05:41 +0200

    ⚡️ Fix AndroidX annotation and FileProvider

--- android/src/main/java/com/jobeso/RNStoryShareModule.java
+++ android/src/main/java/com/jobeso/RNStoryShareModule.java
@@ -5,8 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.annotation.Nullable;
-import android.support.v4.content.FileProvider;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 
 import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
 import com.facebook.react.bridge.Promise;
